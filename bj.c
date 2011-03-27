3705c9255fd238a5cd53048fca145f07a3a2c465
Charl Wang <sfzy_01@163.com>
2011-03-27 11:01:39 +0800

    first

---
+++ bj.c
@@ -0,0 +1,23 @@
+t throw=git reset --hard HEAD 
+git throwh=git reset --hard HEAD^  删除当前git
+git add #$#^^#$%
+git ci = git commit -a -m "#$%$&"
+git diff
+git clone +URL
+vim .vimrc vim文件配置
+set autoindent vim 配置自动缩进 
+go to github  search happypeter-vim
+rm  -f  文件夹名字  f =  force
+   -r    递归 忽略 文件夹内容   r = recursive   rf可同时用
+   gg=G  对齐
+   yy 拷贝一行    
+   dd 上出一行
+   A  本行末端
+   cc 改变一个文件
+   p 粘贴
+   gg 回到开头
+   ctags 
+   ,ss vim下查出错误单词
+   Ctrl +x +k  显示的单词
+   Ctrl +x +f  调文件名
+   Ctrl +x +l 补全
